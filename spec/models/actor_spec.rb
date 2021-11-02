815097e71652706784cf82ee5e5aebbd79767965
Didi Dodson <mdodson55@gmail.com>
2021-11-02 10:32:35 -0600

    Update actor model with tests

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Actor, type: :model do
     it { should have_many :movie_actors}
     it {should have_many(:movies).through(:movie_actors)}
   end
+
+  describe "methods" do
+    it "sorts actors by age" do
+      actor_1 = Actor.create!(name: "Mike Myers", age: 47)
+      actor_2 = Actor.create!(name: "Cameron Diaz", age: 45)
+      actor_3 = Actor.create!(name: "Betty White", age: 99)
+
+      sorted = Actor.actor_sort
+
+      expect(sorted.first).to eq(actor_2)
+      expect(sorted.last).to eq(actor_3)
+    end
+
+    it "sorts actors by age" do
+      actor_1 = Actor.create!(name: "Mike Myers", age: 47)
+      actor_2 = Actor.create!(name: "Cameron Diaz", age: 45)
+      actor_3 = Actor.create!(name: "Betty White", age: 99)
+
+      expect(Actor.average_age.round(2)).to eq(63.67)
+
+    end
+  end
+
 end
